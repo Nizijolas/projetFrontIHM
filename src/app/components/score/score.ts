4da120877f6ca4b57c0e0bcbdd880c65279d3e16
Nizijolas <nicolasdijoud@yahoo.com>
2025-12-12 13:16:42 +0100

    corrections css

--- src/app/components/score/score.ts
+++ src/app/components/score/score.ts
@@ -7,15 +7,17 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { Partie } from '../../model/partie';
 import { SetPing } from '../../model/set-ping';
 import { Error } from "../error/error";
+import { Success } from '../success/success';
 @Component({
   selector: 'app-score',
-  imports: [Notconnected, DisplayScores, ReactiveFormsModule, Error],
+  imports: [Notconnected, DisplayScores, ReactiveFormsModule, Error, Success],
   templateUrl: './score.html',
   styleUrl: './score.css',
 })
 export class Score {
   showAddPartie = signal(false);
   showError = signal(false);
+  showSuccess = signal(false);
   connexions = inject(Connexions);
   fakeParties = inject(Fakeparties);
   nouvellePartieForm = new FormGroup({
@@ -34,6 +36,7 @@ export class Score {
       return;
     }
     this.showError.set(false);
+    this.showSuccess.set(true);
     let set1: SetPing = new SetPing(this.nouvellePartieForm.value.monScoreSet1 ?? 0, this.nouvellePartieForm.value.sonScoreSet1 ?? 0);
     let set2: SetPing = new SetPing(this.nouvellePartieForm.value.monScoreSet2 ?? 0, this.nouvellePartieForm.value.sonScoreSet2 ?? 0);
     let set3: SetPing = new SetPing(this.nouvellePartieForm.value.monScoreSet3 ?? 0, this.nouvellePartieForm.value.sonScoreSet3 ?? 0);
@@ -41,7 +44,7 @@ export class Score {
     let nouvellePartie:Partie = new Partie( set1, set2, set3, this.nouvellePartieForm.value.nomAdversaire?? "undefined");
     this.fakeParties.fakeparties.unshift(nouvellePartie);
 
-  }
+  } 
 
   toggleShowAddPartie(): void {
     this.showAddPartie.set(!this.showAddPartie());
@@ -52,6 +55,9 @@ export class Score {
     this.showError.set(!this.showError());
   }
 
+  toggleShowSucess(){
+    this.showSuccess.set(!this.showSuccess);
+  }
   invalidScores(): boolean {
     if (this.nouvellePartieForm.value.monScoreSet1 == this.nouvellePartieForm.value.sonScoreSet1 ||
       this.nouvellePartieForm.value.monScoreSet2 == this.nouvellePartieForm.value.sonScoreSet2 ||
